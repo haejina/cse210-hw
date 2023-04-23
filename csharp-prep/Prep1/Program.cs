6499fe139b9c5906ac367e8e3cdb4997a672651f
haejina <haejina@byui.edu>
2023-04-22 18:55:35 -0600

    new

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string name = Console.ReadLine();
+        Console.WriteLine(name);
+
+        Console.WriteLine("What is your last name?");
+        string last = Console.ReadLine();
+        Console.WriteLine(last);
+
+        Console.WriteLine($"Your name is {name}, {name} {last}.");
     }
 }
